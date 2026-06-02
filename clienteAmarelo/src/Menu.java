4bd8e9369e42c560b6eec75a1de1d9db50529ff5
Nichole Nicolini <nchnicolini@gmail.com>
2026-06-02 11:21:34 -0300

    adicionando funcionalidades no menu e métodos

--- clienteAmarelo/src/Menu.java
+++ clienteAmarelo/src/Menu.java
@@ -23,6 +23,38 @@ public class Menu {
             }
     
             scanner.close();
+            
+        System.out.println("Funcionalidades disponíveis:");
+        System.out.println("1. Cadastrar cliente");
+        System.out.println("2. Consultar Dados Pessoais de um Cliente");
+        System.out.println("3. Contagem por Sexo");
+        System.out.println("4. Listar Clientes por Faixa Etária");
+        System.out.println("5. Cliente com Menor/Maior Idade");
+        System.out.println("6. Excluir Cliente");
+        System.out.println("7. Atualizar Dados do Cliente");
+        
+        int operacao = scanner.nextInt();
+        switch (operacao) {
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+        
+            default:
+                break;
+        }
     }
 
    
