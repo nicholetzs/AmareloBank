f1d5fc635822967be076d19006d530778b29806b
Nichole Nicolini <nchnicolini@gmail.com>
2026-06-02 10:49:13 -0300

    identificacao dos integrantes (acertem os nomes)

--- clienteAmarelo/src/Menu.java
+++ clienteAmarelo/src/Menu.java
@@ -1,5 +1,8 @@
 public class Menu {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Integrantes:");
+        System.out.println("Nichole Nicolini");
+        System.out.println("Dyone Andrade");
+        System.out.println("Nicolas");
     }
 }
