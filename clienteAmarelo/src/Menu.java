220c580e837a5ce9926d89a0f028e0c91860a7a8
Nichole Nicolini <nchnicolini@gmail.com>
2026-06-02 11:10:02 -0300

    senha e usuario concedidos

--- clienteAmarelo/src/Menu.java
+++ clienteAmarelo/src/Menu.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class Menu {
     public static void main(String[] args) throws Exception {
+        Scanner scanner  = new Scanner(System.in);
         System.out.println("Integrantes:");
         System.out.println("Nichole Nicolini");
         System.out.println("Dyone Andrade");
         System.out.println("Nicolas");
+
+        System.out.println("Bem-vindo ao AmareloBank, digite o número e senha de acesso para entrar na sua conta:");
+        
+        System.out.println("Número:");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Senha:");
+        String senha = scanner.nextLine();
+
+            if (numero == 123456 && senha.equals("admin")) {
+                System.out.println("Acesso concedido. Bem-vindo ao AmareloBank!");
+            } else {
+                System.out.println("Número ou senha incorretos. Acesso negado.");
+            }
+    
+            scanner.close();
     }
+
+   
+
 }
