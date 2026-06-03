17a11b0ffc874620c7c5d77ad9d1e6663ee147c2
Nichole Nicolini <nchnicolini@gmail.com>
2026-06-03 16:32:34 -0300

    fazendo a arvore binaria, sem setRqaiz ou setQuantNos porque nao é modificável extrenamente, apenas detalhes internos da arvore e apenas métodos de operação sabem como modificá-los

--- clienteAmarelo/src/ArvoreBinariaPesquisa.java
+++ clienteAmarelo/src/ArvoreBinariaPesquisa.java
@@ -1,3 +1,21 @@
 public class ArvoreBinariaPesquisa {
+    private No raiz;
+    private int quantNos;
 
+    public ArvoreBinariaPesquisa() {
+        this.quantNos = 0;
+        this.raiz = null;
+    }
+
+    public boolean eVazia() {
+        return this.raiz == null;
+    }
+
+    public No getRaiz() {
+        return this.raiz;
+    }
+
+    public int getQuantNos() {
+        return this.quantNos;
+    }
 }
