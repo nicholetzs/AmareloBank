b5613f2e9778ae77d4d753ab7ed23b94d9083c4f
Nichole Nicolini <nchnicolini@gmail.com>
2026-06-03 12:29:02 -0300

    dúvida: nome vai ser setter ou nao?

--- clienteAmarelo/src/Cliente.java
+++ clienteAmarelo/src/Cliente.java
@@ -1,3 +1,48 @@
 public class Cliente {
+    private String nome;
+    private String cpf;
+    private int idade;
+    private String sexo;
+    private Double saldoCliente;
+
+    public Cliente(String nome, String cpf, int idade, String sexo, Double saldoCliente) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.saldoCliente = saldoCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public Double getSaldoCliente() {
+        return saldoCliente;
+    }
+
+    public void setSaldoCliente(Double saldoCliente) {
+        this.saldoCliente = saldoCliente;
+    }
 
 }
--- clienteAmarelo/src/No.java
+++ clienteAmarelo/src/No.java
@@ -1,3 +1,36 @@
+
 public class No {
+    private Cliente cliente;
+    private No esq, dir;
+
+    public No(Cliente elemento) {
+        this.cliente = elemento;
+        this.esq = null;
+        this.dir = null;
+    };
+
+    public No getEsq() {
+        return this.esq;
+    }
+
+    public No setEsq(No no) {
+        return this.esq = no;
+    }
+
+    public No getDir() {
+        return this.dir;
+    }
+
+    public No setDir(No no) {
+        return this.dir = no;
+    }
+
+    public Cliente getCliente() {
+        return this.cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 
 }
